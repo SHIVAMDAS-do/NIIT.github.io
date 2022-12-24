eecf6df68ca9809746b3c158f79da63fd8895c56
SHIVAMDAS-do <80214397+SHIVAMDAS-do@users.noreply.github.com>
2022-12-24 09:10:31 +0530

    MY NIIT WEBSITE

---
+++ from-handler.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$Subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+
+$email_from = 'info@niit.com'
+$email_subject = 'New From Subnission'
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "User Name: $subject.\n".
+
+
+?>
